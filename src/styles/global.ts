d7145fb51d278d2175233fac09feb417dfd14ea8
gudusol <remicon99@gmail.com>
2024-05-19 05:19:04 +0900

    feat: 스크롤바 삭제

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -25,11 +25,16 @@ const GlobalStyle = css`
   }
   html {
     font-size: 62.5%;
+    scrollbar-width: none;
   }
 
   #root {
     width: 37.5rem;
-    min-height: 100vh;
+    min-height: 200vh;
+  }
+
+  ::-webkit-scrollbar {
+    display: none;
   }
 `;
 
