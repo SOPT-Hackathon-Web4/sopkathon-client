67d6ceefcc82d64de4d787e821553d442f43a955
장정안 <wjddks96@naver.com>
2024-05-18 22:15:40 +0900

    chore: svgr 세팅 및 예시 svg파일 삽입

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react-swc';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+        memo: true,
+      },
+    }),
+  ],
 });
