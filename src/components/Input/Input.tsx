8f3592970611c930b0028c5a6df4fef2d5458776
gudusol <remicon99@gmail.com>
2024-05-19 02:02:36 +0900

    feat: Input 컴포넌트 추가

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,53 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import { Generators } from '@styles/generator';
+import { SerializedStyles } from '@typings/commonType';
+
+export interface InputProps {
+  type?: string;
+  placeholder?: string;
+  value: string;
+  isError?: boolean;
+  errorMessage?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  customStyle?: SerializedStyles;
+}
+
+export interface InputContainerProps {
+  customStyle?: SerializedStyles;
+}
+
+const Input = ({
+  type = 'text',
+  placeholder,
+  value,
+  isError,
+  errorMessage,
+  onChange = () => {},
+  customStyle = css``,
+  ...props
+}: InputProps) => {
+  return (
+    <InputContainer className={`input`}>
+      <input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        customStyle={customStyle}
+        {...props}
+      />
+      {isError && <span>{errorMessage}</span>}
+    </InputContainer>
+  );
+};
+
+export default Input;
+
+const InputContainer = styled.div<InputContainerProps>`
+  width: 100%;
+  ${Generators.flexGenerator()};
+  gap: 0.4rem;
+
+  ${(props) => props.customStyle}
+`;
