fb13620fee15a4de6bd47737b26208d3be3c7b2d
장정안 <wjddks96@naver.com>
2024-05-19 05:12:11 +0900

    feat: 공통 버튼 구현

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 import { Generators } from '@styles/generator';
 import { SerializedStyles } from '@typings/commonType';
 
-export interface ButtonProps {
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   label?: string;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   customStyle?: SerializedStyles;
@@ -38,38 +38,21 @@ const ButtonContainer = styled.button<ButtonContainerProps>`
 
   ${(props) => props.customStyle}
 
-  ${({ $isActivated, theme: { colors } }) =>
+  ${({ theme }) => theme.font.body2};
+
+  ${({ $isActivated, theme: { color } }) =>
     $isActivated
       ? `
   
       border-radius: 12px;
-background: var(--primary1, #FFC4E0);
+background:${color.primary1} ;
+color:${color.gray07}
 
-/* glassmorphism */
-box-shadow: 0px 4px 4px 0px rgba(255, 255, 255, 0.25);
-backdrop-filter: blur(10px);
  `
       : `
       border-radius: 12px;
-border: 1px solid var(--gradient, #EAEAEA);
-background: var(--gray3_BEBEBE, #BEBEBE);
+background: ${color.gray03};
+color:${color.gray01}
 
-/* glassmorphism */
-box-shadow: 0px 4px 4px 0px rgba(255, 255, 255, 0.25);
-backdrop-filter: blur(10px);
  `};
 `;
-
-// ${({ $isActivated, theme: { colors } }) =>
-//   $isActivated
-//     ? `
-//  background-color: ${colors.P_06};
-//  color: ${colors.white};
-//  border: none;
-//  outline: none;
-// `
-//     : `
-//  background-color: ${colors.G_02};
-//  color: ${colors.G_07};
-//  border: none;
-// `};
