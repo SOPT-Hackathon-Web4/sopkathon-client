ef7294a6834b4a2fea9d54853d64d1780c408238
gudusol <remicon99@gmail.com>
2024-05-19 08:05:43 +0900

    import 삭제

--- src/assets/svg/IcResultImg.tsx
+++ src/assets/svg/IcResultImg.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import type { SVGProps } from 'react';
 const SvgIcResultImg = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 103 161" {...props}>
