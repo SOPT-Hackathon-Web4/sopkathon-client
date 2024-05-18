8af0b702cc190f6a0ca4e76d158122f0d2f277e8
장정안 <wjddks96@naver.com>
2024-05-19 00:54:28 +0900

    feat: 라우터 로그인페이지 추가

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { Home } from '@pages';
+import LoginPage from './pages/login/LoginPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
   },
 ]);
 
