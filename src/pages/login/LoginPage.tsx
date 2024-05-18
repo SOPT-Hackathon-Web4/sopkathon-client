105b0296ec0ee7cf2f3cf1c3f8b616d3cf14f2b9
장정안 <wjddks96@naver.com>
2024-05-19 04:26:57 +0900

    feat: 로그인페이지에서 중복된 인스타아이디면 분기처리

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -4,14 +4,16 @@ import PasswordInput from './components/PasswordInput';
 import NameInput from './components/NameInput';
 import { useState } from 'react';
 import axios from 'axios';
+import ExistingPasswordInput from './components/ExistingPasswordInput';
 
-const LOGIN_FORM_STEP = ['ID', 'PASSWORD', 'NAME'] as const;
+const LOGIN_FORM_STEP = ['ID', 'PASSWORD', 'EXISTING_PASSWORD', 'NAME'] as const;
 
 const LoginPage = () => {
   const { Funnel, setStep } = useFunnel(LOGIN_FORM_STEP, LOGIN_FORM_STEP[0]);
 
   const [instaValue, setInstaValue] = useState('');
   const [password, setPassword] = useState('');
+  const [existingPassword, setExistingPassword] = useState('');
 
   const [name, setName] = useState('');
 
@@ -23,6 +25,10 @@ const LoginPage = () => {
   };
   const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setName(e.target.value);
+  };
+
+  const handleExistingPasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setExistingPassword(e.target.value);
   };
 
   const handleSubmit = async () => {
@@ -41,28 +47,44 @@ const LoginPage = () => {
     }
   };
 
-  // const handleDuplicatedInstaId = () => {
-  // try {
-  //       /**@todo 정안 api 바꾸기 */
-  //       const response = await axios.get(`/your-api-endpoint?instaId=${instaValue}`);
-  //       if (response.data === true) {
-  //         onNext();
-  //       } else {
-  //         /**@todo 정안 api 바꾸기 */
-  //         navigate('/some-other-page');
-  //       }
-  //     } catch (error) {
-  //       console.error('Error checking Instagram ID:', error);
-  //     }
-  // };
+  const handleDuplicatedInstaId = async () => {
+    try {
+      /**@todo 정안 api 바꾸기 */
+      setStep(() => 'PASSWORD');
+
+      // const response = await axios.get(`/your-api-endpoint?instaId=${instaValue}`);
+      // if (response.data === true) {
+      //   setStep(() => 'PASSWORD');
+      // } else {
+      //   /**@todo 정안 api 바꾸기 */
+      // }
+    } catch (error) {
+      setStep(() => 'EXISTING_PASSWORD');
+      console.error('Error checking Instagram ID:', error);
+    }
+  };
 
   return (
     <Funnel>
       <Funnel.Step name="ID">
-        <InstaInput onNext={() => setStep(() => 'PASSWORD')} instaValue={instaValue} onChange={handleInputChange} />
+        <InstaInput
+          onNext={() => {
+            handleDuplicatedInstaId();
+            // setStep(() => 'PASSWORD');
+          }}
+          instaValue={instaValue}
+          onChange={handleInputChange}
+        />
       </Funnel.Step>
       <Funnel.Step name="PASSWORD">
         <PasswordInput onNext={() => setStep(() => 'NAME')} password={password} onChange={handlePasswordChange} />
+      </Funnel.Step>
+      <Funnel.Step name="EXISTING_PASSWORD">
+        <ExistingPasswordInput
+          onNext={() => setStep(() => 'NAME')}
+          existingPassword={existingPassword}
+          onChange={handleExistingPasswordChange}
+        />
       </Funnel.Step>
       <Funnel.Step name="NAME">
         <NameInput onNext={handleSubmit} name={name} onChange={handleNameChange} />
